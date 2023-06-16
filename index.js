24459b9eb046b078203d58628d1a769c8ac5178a
Mohamed Farraj <mohamed.farraj@3ddx.com>
2023-06-16 13:21:50 +0200

    add

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function createWindow() {
         }),
     );
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
     mainWindow.on('closed', function() {
         mainWindow = null;
