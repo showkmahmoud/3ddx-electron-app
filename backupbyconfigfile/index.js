4d7347f421672489cd28bd694fccea995911f704
Mohamed Farraj <mohamed.farraj@3ddx.com>
2023-06-25 17:23:04 +0200

    add

--- backupbyconfigfile/index.js
+++ backupbyconfigfile/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, autoUpdater } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const url = require('url');
 const path = require('path');
 
@@ -26,10 +26,6 @@ function createWindow() {
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
-
-    setTimeout(() => {
-        checkNewUpdates();
-    }, 3000);
 }
 
 app.on('ready', createWindow);
@@ -75,23 +71,12 @@ var request = require('request');
 const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
+    const data = fs.readFileSync(path.join(__dirname, '/config.json'), 'utf8');
     if (req.url == '/config.json') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
+        console.log(JSON.stringify(configData))
         res.end(JSON.stringify(configData));
-    } else if (req.url == '/saveConfig') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        let body = [];
-        req
-            .on('data', (chunk) => {
-                body.push(chunk);
-            })
-            .on('end', () => {
-                body = Buffer.concat(body).toString();
-                configData = JSON.parse(body);
-                res.end(body);
-            });
     } else if (req.url == '/upload') {
         const formData = {
             myfile: fs.createReadStream(JSON.parse(data).filePath),
@@ -112,6 +97,24 @@ const server = http.createServer((req, res) => {
         //         }
         //     },
         // );
+    } else if (req.url == '/saveConfig') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        let body = [];
+        req
+            .on('data', (chunk) => {
+                body.push(chunk);
+            })
+            .on('end', () => {
+                body = Buffer.concat(body).toString();
+                const data = fs.writeFileSync(
+                    path.join(__dirname, '/config.json'),
+                    body,
+                    'utf8',
+                );
+
+                res.end(body);
+            });
     } else {
         res.statusCode = 200;
         res.write('Hello World!'); //write a response to the client
@@ -123,30 +126,20 @@ server.listen(port, () => {
     console.log(`Server running at http://${port}/`);
 });
 
+const { autoUpdater } = require('electron-updater');
+
 ipcMain.on('app_version', (event) => {
     event.sender.send('app_version', { version: app.getVersion() });
 });
 
-let updateUrl = null;
-let checkNewUpdates = () => {
-    const host = 'https://alwanlab.com';
-    const feed = `${host}/3ddx/${process.platform}-${process.arch}.json`;
-    request.get(feed, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
+autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update_available');
+});
 
-            if (JSON.parse(body).name != app.getVersion()) {
-                updateUrl = JSON.parse(body).url;
-                console.log(updateUrl);
+autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update_downloaded');
+});
 
-                mainWindow.webContents.send('update_available');
-            }
-        }
-    });
-};
-
-
-
-ipcMain.on('update_app', () => {
-    console.log(updateUrl);
-    mainWindow.webContents.downloadURL(updateUrl);
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
 });
