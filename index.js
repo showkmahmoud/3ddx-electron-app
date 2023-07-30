16fa971c82023bdd42867198da7bc7ed40c52bcc
Mohamed Farraj <alwanlab.com@gmail.com>
2023-07-30 12:20:27 +0300

    change to elctron builder

--- index.js
+++ index.js
@@ -1,183 +1,113 @@
-const { app, BrowserWindow, ipcMain, autoUpdater } = require('electron');
-const url = require('url');
-const path = require('path');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const url = require("url");
+const path = require("path");
 
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 1280,
-        height: 960,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            
-        },
-        autoHideMenuBar: true,
-    });
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 960,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    autoHideMenuBar: true,
+  });
 
-    mainWindow.loadURL(
-        url.format({
-            pathname: path.join(__dirname, `/index.html`),
-            protocol: 'file:',
-            slashes: true,
-        }),
-    );
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
-
-    setTimeout(() => {
-        checkNewUpdates();
-    }, 3000);
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/index.html`),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', function() {
-    if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
 
-app.on('activate', function() {
-    if (mainWindow === null) createWindow();
+app.on("activate", function () {
+  if (mainWindow === null) createWindow();
 });
 
 let configData = {
-    name: 'test name2',
-    filePath: '/home/mohamedfarraj/Desktop/data/',
-    emailAddress: 'sample@3ddx.com',
-    password: 'Sam1@234',
+  name: "test name2",
+  filePath: "/home/mohamedfarraj/Desktop/data/",
+  emailAddress: "sample@3ddx.com",
+  password: "Sam1@234",
 };
 
 let getArguments = () => {
-    const array = process.argv;
+  const array = process.argv;
 
-    for (const item of array) {
-        if (item.includes('=')) {
-            const arr = item.split('=');
-            configData[arr[0]] = arr[1];
-        }
+  for (const item of array) {
+    if (item.includes("=")) {
+      const arr = item.split("=");
+      configData[arr[0]] = arr[1];
     }
-    return array;
+  }
+  return array;
 };
 
-console.log('arrges', getArguments());
+console.log("arrges", getArguments());
 
-//http
-const http = require('http');
+const fs = require("fs");
 
-const port = 3000;
-
-const fs = require('fs');
-
-var request = require('request');
-
-const server = http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    if (req.url == '/config.json') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(configData));
-    } else if (req.url == '/saveConfig') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        let body = [];
-        req
-            .on('data', (chunk) => {
-                body.push(chunk);
-            })
-            .on('end', () => {
-                body = Buffer.concat(body).toString();
-                configData = JSON.parse(body);
-                res.end(body);
-            });
-    } else if (req.url == '/upload') {
-        const formData = {
-            myfile: fs.createReadStream(JSON.parse(data).filePath),
-        };
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        // res.end(formData);
-        // request.post({ url: 'http://localhost:2000/uploadjavatpoint', formData: formData },
-        //     function(error, response, body) {
-        //         if (!error && response.statusCode == 200) {
-        //             res.statusCode = 200;
-        res.write(formData); //write a response to the client
-        res.end();
-        //         } else {
-        //             res.statusCode = 200;
-        //             res.write(error); //write a response to the client
-        //             res.end();
-        //         }
-        //     },
-        // );
-    } else {
-        res.statusCode = 200;
-        res.write('Hello World!'); //write a response to the client
-        res.end();
-    }
-});
-
-server.listen(port, () => {
-    console.log(`Server running at http://${port}/`);
-});
-
-ipcMain.on('app_version', (event) => {
-    event.sender.send('app_version', { version: app.getVersion() });
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
 });
 
 // Back Btn
-ipcMain.on('back', (event) => {
-    mainWindow.loadURL(
-        url.format({
-            pathname: path.join(__dirname, `/index.html`),
-            protocol: 'file:',
-            slashes: true,
-        }),
-    );
+ipcMain.on("back", (event) => {
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/index.html`),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
 });
 
 //Done
-ipcMain.on('done', (event) => {
-    mainWindow.close()
+ipcMain.on("done", (event) => {
+  mainWindow.close();
 });
 
-ipcMain.on('upload', (event, args) => {
-    console.log(event);
-    console.log(args);
-    // event.sender.send('get_data', configData);
-    const file = fs.readFileSync('./test.jpg');
-    const base64Data = file.toString('base64');
-    event.sender.send('uploadFile', {fileName: 'test.jpg', fileContent: base64Data})
+ipcMain.on("upload", (event, args) => {
+  const file = fs.readFileSync("./test.jpg");
+  const base64Data = file.toString("base64");
+  event.sender.send("uploadFile", {
+    fileName: "test.jpg",
+    fileContent: base64Data,
+  });
 });
 
-ipcMain.on('get_data', (event) => {
-    event.sender.send('get_data', configData);
+ipcMain.on("get_data", (event) => {
+  event.sender.send("get_data", configData);
 });
 
+// update
+const { autoUpdater } = require("electron-updater");
+mainWindow.once("ready-to-show", () => {
+  autoUpdater.checkForUpdatesAndNotify();
+});
 
-let updateUrl = null;
-let checkNewUpdates = () => {
-    const host = 'https://alwanlab.com';
-    const feed = `${host}/3ddx/${process.platform}-${process.arch}.json`;
-    request.get(feed, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
+});
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
+});
 
-            if (JSON.parse(body).name != app.getVersion()) {
-                updateUrl = JSON.parse(body).url;
-                console.log(updateUrl);
-
-                mainWindow.webContents.send('update_available');
-            }
-        }
-    });
-};
-
-
-
-ipcMain.on('update_app', () => {
-    mainWindow.webContents.downloadURL(updateUrl);
+ipcMain.on("restart_app", () => {
+  autoUpdater.quitAndInstall();
 });
